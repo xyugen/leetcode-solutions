6feb2400ac363ad83d04caf1471f7a02d8a69940
Renz Arias <alex.arias.jorym@gmail.com>
2023-08-20 10:05:17 +0800

    412 java submission

---
+++ 412/ Fizz Buzz/javasolution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class FizzBuzz {
+    public static void main(String[] args) {
+        //
+    }
+
+    // Submitted 2023-08-20
+    public static List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(String.valueOf(i));
+            }
+        }
+
+        return answer;
+    }
+}
