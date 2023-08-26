257a53a22948972a6a25f18f6d6061ebafff2ec1
Renz Arias <alex.arias.jorym@gmail.com>
2023-08-26 08:34:29 +0800

    2520 java submission 2

--- 2520/ Count the Digits That Divide a Number/javasolution.java
+++ 2520/ Count the Digits That Divide a Number/javasolution.java
@@ -4,18 +4,30 @@ class CountDigits {
     }
 
     // Submitted 2023-08-26
-    public static int countDigits(int num) {
-        if (num <= 9) return 1;
+//    public static int countDigits(int num) {
+//        if (num <= 9) return 1;
+//
+//        int newNum = num;
+//        int divisible = 0;
+//        while (newNum != 0) {
+//            if (num % (newNum % 10) == 0) {
+//                divisible++;
+//            }
+//            newNum /= 10;
+//        }
+//
+//        return divisible;
+//    }
 
-        int newNum = num;
-        int divisible = 0;
-        while (newNum != 0) {
+    // Submitted 2023-08-26
+    public static int countDigits(int num) {
+        int newNum = num, divisible = 0;
+        while (newNum > 0) {
             if (num % (newNum % 10) == 0) {
                 divisible++;
             }
             newNum /= 10;
         }
-
         return divisible;
     }
 }
